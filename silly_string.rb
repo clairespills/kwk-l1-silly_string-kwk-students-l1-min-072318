91a38cd6440d875355b37554980276e39b01a007
clairespills <purpleprincess372@gmail.com>
2018-07-23 19:39:07 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -23,7 +23,7 @@ puts lowercased_phrase
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word.length letters
+letter_count = big_word.length
 
 puts "There are #{letter_count} in #{big_word}"
 
